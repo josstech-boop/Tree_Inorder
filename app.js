5120349a9153db1a09990682ca104f2facc3e1ac
Joselyn Guitz <joselynguitz@gmail.com>
2026-07-01 12:52:36 -0600

    Se realizo funciones para dibujar el arbol

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ class BinarySearchTree {
         let newNode = new Node(value)
         if (this.root === null) {
             this.root = newNode
+            arbol.innerHTML = ''
             this.pintar(this.root)
         } else {
             let currentNode = this.root
@@ -23,12 +24,16 @@ class BinarySearchTree {
                 if (value < currentNode.value) {
                     if (!currentNode.left) {
                         currentNode.left = newNode
+                        arbol.innerHTML = ''
+                        this.pintar(this.root)
                         return this
                     }
                     currentNode = currentNode.left
                 } else {
                     if (!currentNode.right) {
                         currentNode.right = newNode
+                        arbol.innerHTML = ''
+                        this.pintar(this.root)
                         return this
                     }
                     currentNode = currentNode.right
@@ -56,15 +61,33 @@ class BinarySearchTree {
     }
 
     pintar(node) {
+        //varible temporal
+        let nodeTemporal = node
+        let nodoIzquierda;
+        let nodoDerecha;
+
+        if (nodeTemporal.left != null) {
+            nodoIzquierda = this.pintar(nodeTemporal.left)
+        }
+
+        if (nodeTemporal.right != null) {
+            nodoDerecha = this.pintar(nodeTemporal.right)
+        }
+
+        if (nodeTemporal.left == null && nodeTemporal.right == null) {
+            nodoIzquierda = ''
+            nodoDerecha = ''
+        }
         //padre
         let padre = document.createElement('li')
-        padre.setAttribute('data-indice', `${node.value}`)
+        padre.setAttribute('data-indice', `${nodeTemporal.value}`)
         //indice
-        let div = document.createElement('div')
+        let div = document.createElement('span')
         div.className = 'nodo-wrapper'
-        let hijo = document.createElement('div')
-        hijo.className = 'nodo-circulo nodo-azul'
-        hijo.textContent = node.value
+
+        let indice = document.createElement('div')
+        indice.className = 'nodo-circulo nodo-azul'
+        indice.textContent = nodeTemporal.value
 
         //hijos
         let hijos = document.createElement('ul')
@@ -73,41 +96,22 @@ class BinarySearchTree {
         let right = document.createElement('li')
         right.className = 'derecha'
 
+
+        if (nodeTemporal.left != null) {
+            left.append(nodoIzquierda)
+        }
+        if (nodeTemporal.right != null) {
+            right.append(nodoDerecha)
+        }
+
+        div.append(indice)
+        padre.append(div)
         hijos.append(left)
         hijos.append(right)
-
-        div.append(hijo)
-
-        padre.append(div)
         padre.append(hijos)
-
-        console.log(padre)
-
         arbol.append(padre)
 
-        console.log(hijo.parentElement.parentElement)
-
-        console.log(arbo)
-
-
-        // arbol.innerHTML = `<li>
-        //                         <div class="nodo-wrapper">
-        //                             <div class="nodo-circulo nodo-azul">${this.root.value}</div>
-        //                         </div>
-        //                     </li>
-
-        //                     <li>
-        //                         <div class="nodo-wrapper">
-        //                             <div class="nodo-circulo nodo-azul">${this.root.value}</div>
-        //                         </div>
-        //                     </li>
-
-        //                     <li>
-        //                         <div class="nodo-wrapper">
-        //                             <div class="nodo-circulo nodo-azul">${this.root.value}</div>
-        //                         </div>
-        //                     </li>
-        //                     `
+        return padre
     }
 
 }
@@ -116,16 +120,24 @@ class BinarySearchTree {
 
 let inputIngreso = document.querySelector('#inputIngreso')
 let btnGuardar = document.querySelector('#guardar')
-let posiciones = document.querySelectorAll('.nodo-wrapper')
 let arbol = document.querySelector('.arbol')
+let posiciones
+
+let btnBuscar = document.querySelector('#buscar')
+let inputBuscar = document.querySelector('#inputBuscar')
 
 const Tree = new BinarySearchTree()
 
 btnGuardar.addEventListener('click', (event) => {
-
-    Tree.insert(parseFloat(inputIngreso.value))
+    Tree.insert(Number(inputIngreso.value))
     inputIngreso.value = ''
+    posiciones = document.querySelectorAll('[data-indice]')
 
+})
+
+btnBuscar.addEventListener('click', (event) => {
+    btnGuardar.disabled = true
+    console.log(inputBuscar.value)
 })
 
 
